9a5194d3a8abd5802fff95918c8811efe28527f3
Mykyta Krainik <nikinta2003@gmail.com>
2024-03-10 11:21:17 +0200

    refactor: update the example of using C++ in JS

--- js-test/binding.gyp
+++ js-test/binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "SwigJS",
+      "target_name": "CarJS",
       "sources": [ "../car_wrap.cxx", "../car.cpp" ],
       "include_dirs" : [ "<!(node -e \"require('nan')\")" ]
     }
--- js-test/index.js
+++ js-test/index.js
@@ -1,6 +1,22 @@
-const Car = require('./build/Release/SwigJS');
+const {car: Car} = require('./build/Release/CarJS');
 
-const car = Car.get_car(2020, "X5", 7.5, 2.0);
-car.print();
+const car = new Car(2020, "X5", 7.5, 2.0);
 
-console.log(typeof car.get_year())
+const printCar = (car) => {
+  console.log('********************');
+  car.print();
+  console.log('year', typeof car.get_year(), car.get_year());
+  console.log('model', typeof car.get_model(), car.get_model());
+  console.log('petrol_consumption', typeof car.get_petrol_consumption(), car.get_petrol_consumption());
+  console.log('engine_volume', typeof car.get_engine_volume(), car.get_engine_volume());
+  console.log('********************');
+};
+
+printCar(car);
+
+car.set_year(2021);
+car.set_model("X6");
+car.set_petrol_consumption(8.5);
+car.set_engine_volume(2.5);
+
+printCar(car);
